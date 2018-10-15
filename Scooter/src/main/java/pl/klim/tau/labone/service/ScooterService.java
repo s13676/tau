24f7882ef14687c047d905e062758e50e07d9c05
lukas <lukasz.klim92@gmail.com>
2018-10-15 22:16:46 +0200

    Interface and empty implementation

---
+++ Scooter/src/main/java/pl/klim/tau/labone/service/ScooterService.java
@@ -0,0 +1,13 @@
+package pl.klim.tau.labone.service;
+
+import pl.klim.tau.labone.domain.Scooter;
+
+import java.util.ArrayList;
+
+public interface ScooterService {
+    void create(Scooter scooter);
+    ArrayList<Scooter> readAll();
+    Scooter read(int id);
+    void update(Scooter scooter);
+    void delete(Scooter scooter);
+}
---
+++ Scooter/src/main/java/pl/klim/tau/labone/service/ScooterServiceImpl.java
@@ -0,0 +1,29 @@
+package pl.klim.tau.labone.service;
+
+import pl.klim.tau.labone.domain.Scooter;
+
+import java.util.ArrayList;
+
+public class ScooterServiceImpl implements ScooterService {
+    private ArrayList<Scooter> scooters = new ArrayList<Scooter>();
+
+    public void create(Scooter scooter) {
+
+    }
+
+    public ArrayList<Scooter> readAll() {
+        return null;
+    }
+
+    public Scooter read(int id) {
+        return null;
+    }
+
+    public void update(Scooter scooter) {
+
+    }
+
+    public void delete(Scooter scooter) {
+
+    }
+}
